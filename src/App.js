f701a131d3a7d525060c5a429fdb4fedaa19dcfd
Sajib Saha <sajibsaha859@gmail.com>
2023-03-26 05:08:31 +0530

    Showing name phone

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React,{Component} from "react";
+import React,{Component, useState}from "react";
+import Header from "./Header";
 class App extends Component{
   render(){
     return(
@@ -10,18 +11,49 @@ class App extends Component{
     );
   }
 }
+class App3 extends Component{
+  render(){
+  return(
+ <div>
+<label htmlFor="username"> Username: </label> <br/>
+<input id="username" type="text"/><br/>
+<label htmlFor="password">Password: </label><br/>
+<input id="password" type="password"/><br/>
+</div>
+);}
+}
+//export default App3;
 function App1() {
+  //let x=3;
+  //let y=2;
+  const [name,setName]=useState('');
+  let [phone,setPhone]=useState('');
+  const handleName=(event)=> {setName(event.target.value);};
+  const handlePhone=(event)=>{setPhone(parseInt(event.target.value));};
+  if (isNaN(phone)){
+    phone='';
+  }
   return (
-    <div>    <div>
-     Phone Directory
-    </div>
-     <button>Add</button>
+    <div> 
+      <Header/> 
+      <input type="text" placeholder="Type your Name" value={name} onChange={handleName} id='nameid'/><br/>
+      <input type="text" placeholder="Type your Number" value={phone} onChange={handlePhone} id='phoneid'/><br/>
+      <button>Add</button>
     <div>
-      <span >Name:</span>
-      <span>Phone:</span>
+      <span >Name: {name}</span>
+      <br/>
+      <span>Phone: {phone}</span>
+      
     </div>
-    </div>
-
+    
+  
+  {
+    // <label htmlFor="name">Name:</label>
+    // <input id="name" type="text" placeholder="your name" defaultValue={x+y}/>
+    // </div>
+    // /* JSX is case sensitive */
+  }
+   </div>
   );
 }
 
