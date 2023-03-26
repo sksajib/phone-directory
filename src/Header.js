d1ba0d61d1f9599997233b01e87ae61a19db8699
Sajib Saha <sajibsaha859@gmail.com>
2023-03-26 05:33:39 +0530

    change header

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 function Header(){
     return(
        <div>
-        <h1 style={{background:"black",padding:20,color:"white",textTransform:"uppercase",textAlign:"center"}}>Phone Directory</h1>
+        <h1 style={{background:"black",padding:20,color:"white",textTransform:"uppercase",textAlign:"center"}}>Phone Directory PRototype</h1>
        </div>
 
     );
